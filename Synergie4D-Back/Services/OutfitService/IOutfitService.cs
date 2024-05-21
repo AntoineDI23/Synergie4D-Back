e0a8c386a1fffca94950d2df6e88c191a648194d
AntoineDI23 <antoine.simeon@viacesi.fr>
2024-05-21 15:41:57 +0200

    [Feat] Add Outfit services

---
+++ Synergie4D-Back/Services/OutfitService/IOutfitService.cs
@@ -0,0 +1,17 @@
+﻿using Synergie4D_Back.Models.Outfit;
+
+namespace Synergie4D_Back.Services.OutfitService
+{
+    public interface IOutfitService
+    {
+        #region CRUD Outfit
+
+        void AddAllOutfits();
+
+        Outfit GetOutfitById(int outfitId);
+
+        IEnumerable<Outfit> GetAllOutfits();
+
+        #endregion CRUD Outfit
+    }
+}
---
+++ Synergie4D-Back/Services/OutfitService/OutfitService.cs
@@ -0,0 +1,22 @@
+﻿using Synergie4D_Back.Models.Outfit;
+
+namespace Synergie4D_Back.Services.OutfitService
+{
+    public class OutfitService : IOutfitService
+    {
+        public void AddAllOutfits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Outfit> GetAllOutfits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Outfit GetOutfitById(int outfitId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
