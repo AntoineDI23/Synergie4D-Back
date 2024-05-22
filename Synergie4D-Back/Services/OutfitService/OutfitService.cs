3f12584193329485e5b1e1a21c9c8323abda812a
AntoineDI23 <antoine.simeon@viacesi.fr>
2024-05-22 08:31:46 +0200

    [Feat] Add outfits examples

--- Synergie4D-Back/Services/OutfitService/OutfitService.cs
+++ Synergie4D-Back/Services/OutfitService/OutfitService.cs
@@ -13,6 +13,48 @@ namespace Synergie4D_Back.Services.OutfitService
                 Style = Style.Relaxed,
                 Weather = Weather.Hot
             },
+            new() {
+                Id = 2,
+                Description = "T-shirt / sweat / jean / basket",
+                Style = Style.Relaxed,
+                Weather = Weather.Cold
+            },
+            new() {
+                Id = 3,
+                Description = "Chemise / pantalon / chaussure de ville",
+                Style = Style.Professional,
+                Weather = Weather.Cold
+            },
+            new() {
+                Id = 4,
+                Description = "Polo / pantalon / basket",
+                Style = Style.Professional,
+                Weather = Weather.Hot
+            },
+            new() {
+                Id = 5,
+                Description = "T-shirt / short / basket",
+                Style = Style.Sporty,
+                Weather = Weather.Hot
+            },
+            new() {
+                Id = 6,
+                Description = "Costume / chaussure de ville",
+                Style = Style.Professional,
+                Weather = Weather.Cold
+            },
+            new() {
+                Id = 7,
+                Description = "T-shirt / pull / pantalon / chaussure de ville",
+                Style = Style.Relaxed,
+                Weather = Weather.Cold
+            },
+            new() {
+                Id = 8,
+                Description = "T-shirt / sweat / jogging / basket",
+                Style = Style.Sporty,
+                Weather = Weather.Hot
+            }
         ];
 
         public IEnumerable<Outfit> GetAllOutfits()
